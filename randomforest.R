3efc0b9a1dd77503eee77482ec4abb185eeef70a
Ryan Sun <bfdnd@hotmail.com>
2018-11-14 14:54:04 -0600

    Edit for random forest

--- randomforest.R
+++ randomforest.R
@@ -1,49 +1,34 @@
-
-RandomForest<- function(data, storeID, categoryID) {
-  require(glmnet)
+RandomForest <- function(data, storeID, categoryID) {
+  
   require(dplyr)
-data %>% filter(StoreID == storeID) -> storeData
-storeData %>% filter(Random == 'Train') -> train
-storeData %>% filter(Random == 'Test') -> test
-
-train %>% select(matches('^[DPF].$')) %>% as.matrix() -> trainX
-train %>% select(paste0('Y', categoryID)) %>% as.matrix() -> trainY
-train_data = data.frame(trainX,trainY)
-
-
-test %>% select(matches('^[DPF].$')) %>% as.matrix() -> testX
-test %>% select(paste0('Y', categoryID)) %>% as.matrix() -> testY
-test_data = data.frame(testX,testY)
-
-Y = paste0('Y', categoryID)
-
-data1 <- merge(train_data,test_data,all = TRUE)
-
-library(MASS)
-
-library(randomForest)
-
-set.seed(2)
-
-train1 = sample(1:nrow(data1),nrow(data1)/2)
-
-data.test=data1[-train1,Y]
-
-set.seed(2)
-# MSE is 131756.5
-rf.sample = randomForest(Y~.,data=data1,subset=train1,mtry=3,importance = TRUE)
-# randomly choose 3 variables
-
-yhat.rf = predict(rf.sample,newdata = data1[-train1,])
-
-mean((yhat.rf-data.test)^2)
-
-varImpPlot(rf.sample)
-
-importance(rf.sample)
-
-return(list(model = rf.sample, MSE = mean((yhat.rf-data.test)^2)))
+  require(randomForest)
+  
+  data %>% filter(StoreID == storeID) -> storeData
+  storeData %>% filter(Random == 'Train') -> train
+  storeData %>% filter(Random == 'Test') -> test
+  
+  train %>% select(matches('^[DPF].$')) -> trainX
+  train %>% select(paste0('Y', categoryID)) -> trainY
+  
+  trainData <- data.frame(trainX,trainY)
+  
+  test %>% select(matches('^[DPF].$')) -> testX
+  test %>% select(paste0('Y', categoryID)) -> testY
+  
+  testData <- data.frame(testX,testY)
+  
+  Y <- paste0('Y', categoryID)
+  # 
+  # data1 <- merge(trainData,testData,all = TRUE)
+  # train1 = sample(1:nrow(data1),nrow(data1)/2)
+  # 
+  # dataTest=data1[-train1,Y]
+  
+  rf <- randomForest(as.formula(paste0(Y, '~.')),data=trainData,mtry=3,importance = TRUE)  # randomly choose 3 variables
+  yhat <- predict(rf,newdata = testData)
+  
+  return(list(model = rf, MSE = mean((unlist(yhat - testY)^2))))
 }
+
 RandomForest(allData, 3, 1)
-```
 
