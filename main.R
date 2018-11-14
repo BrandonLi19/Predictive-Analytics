1295275580b4a20c53e4ae37c29de247c218ce35
Ryan Sun <bfdnd@hotmail.com>
2018-11-13 22:59:48 -0600

    Removed Ridge Regression def as it is now in archive

--- main.R
+++ main.R
@@ -12,32 +12,7 @@ ModelEval <- function(pair, modelList, data){
 }
 
 
-RidgeRegression <- function(data, storeID, categoryID) {
-  
-  require(glmnet)
-  require(dplyr)
-  
-  data %>% filter(StoreID == storeID) -> storeData
-  storeData %>% filter(Random == 'Train') -> train
-  storeData %>% filter(Random == 'Test') -> test
-  
-  train %>% select(matches('^[DPF].+$')) %>% as.matrix()  -> trainX
-  train %>% select(paste0('Y', categoryID)) %>% as.matrix()  -> trainY
-  
-  
-  test %>% select(matches('^[DPF].+$')) %>% as.matrix() -> testX
-  test %>% select(paste0('Y', categoryID)) %>% as.matrix()  -> testY
-  
-  # Begin ridge regression
-  grid <- 10^seq(10,-2,length=100)
-  
-  ridgeModel <- cv.glmnet(trainX, trainY, alpha=0, lambda=grid)
-  bestLambda <- ridgeModel$lambda.min
-  prediction <- predict(ridgeModel, s=bestLambda, newx=testX)
-  
-  return(list(model = ridgeModel, MSE = mean((prediction - testY) ^ 2)))
-  
-}
+
 
 
 # Read all xlsx workbooks
