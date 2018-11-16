192da643e130d8eaa09d243ca6f2ad2d5d089112
Ryan Sun <bfdnd@hotmail.com>
2018-11-16 05:54:46 -0600

    Corrected bug that does not return MSE.

--- main.r
+++ main.r
@@ -1,10 +1,27 @@
-
+## finds all .R files within a folder and soruces them
+SourceEntireFolder <- function(folderName, verbose=FALSE, showWarnings=TRUE) { 
+  files <- list.files(folderName, full.names=TRUE)
+  
+  # Grab only R files
+  files <- files[ grepl("\\.[rR]$", files) ]
+  
+  if (!length(files) && showWarnings)
+    warning("No R files in ", folderName)
+  
+  for (f in files) {
+    if (verbose)
+      cat("sourcing: ", f, "\n")
+    ## TODO:  add caught whether error or not and return that
+    try(source(f, local=FALSE, echo=FALSE), silent=!verbose)
+  }
+  return(invisible(NULL))
+}
 
 ModelEval <- function(pair, modelList, data){
   evalResult <- lapply(modelList, function(f) {do.call(f, list(data = data,
                                                               storeID = pair['store'],
                                                               categoryID = pair['category']))})
-  bestModelIndex <- which.min(unlist(lapply(evalResult, function(entry) {entry[[1]][['MSE']]})))
+  bestModelIndex <- which.min(unlist(lapply(evalResult, function(entry) {entry$MSE})))
   bestModel <- evalResult[[bestModelIndex]]
   bestModel$storeID <- pair['store']
   bestModel$categoryID <- pair['category']
@@ -40,5 +57,8 @@ categoryList <- seq.int(from = 1, to = 24)
 storeList <- seq.int(from = 2, to = 9)
 combinations <- expand.grid(store = storeList, category = categoryList)
 
-modelList <- list(RidgeRegression)
-apply(combinations, MARGIN = 1, ModelEval, modelList = modelList, data = allData)
+SourceEntireFolder('mkt500s')
+
+# modelList <- list(LassoRegression, BoostedTree, NeuralNetwork, RandomForest)
+modelList <- list(LassoRegression, BoostedTree, RandomForest)
+fr <- apply(combinations, MARGIN = 1, ModelEval, modelList = modelList, data = allData)
