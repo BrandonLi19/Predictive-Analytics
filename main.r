228e6c7b6b72e312c2a66135e46ea3c29b418a2a
Ryan Sun <bfdnd@hotmail.com>
2018-11-16 06:55:05 -0600

    Separated data prep function

--- main.r
+++ main.r
@@ -32,25 +32,6 @@ ModelEval <- function(pair, modelList, data){
 
 
 
-# Read all xlsx workbooks
-library(readxl)
-
-fileList <- list.files(pattern = '*.xlsx')
-dataList <- sapply(fileList, read_xlsx, simplify = FALSE)
-names(dataList) <- tolower(substr(names(dataList), 1, 6))  # Rename list items to 'store2'-like
-
-# GetStoreIDMapping <- function(dataList) {
-#   # May not be necessary
-#   lapply(dataList, function(dataFrame) {unique(dataFrame$Store)})
-# }
-
-# Concatenate store data and add an additional column StoreID ranging from 2 to 9
-addIDColumn <- function(storeID) {
-  return(cbind(dataList[[storeID]], StoreID = substr(storeID, 6, 6)))
-}
-
-dataFrameWithStoreID <- lapply(names(dataList), addIDColumn)
-allData <- do.call(rbind, dataFrameWithStoreID)
 
 # Generate all combinations of categories and stores
 categoryList <- seq.int(from = 1, to = 24)
